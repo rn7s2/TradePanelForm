30834df275d430c5e9d83184b8c64502bb4c6763
rn7s2 <mail@rn7s2.cn>
2025-08-15 13:08:16 +0800

    feat: hide close box

--- TradePanel/TradePanelForm.cs
+++ TradePanel/TradePanelForm.cs
@@ -1,13 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
 namespace TradePanel
 {
-    
+
     public partial class TradePanelForm : Form
     {
+        private const int CP_NOCLOSE_BUTTON = 0x200;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams myCp = base.CreateParams;
+                myCp.ClassStyle = myCp.ClassStyle | CP_NOCLOSE_BUTTON;
+                return myCp;
+            }
+        }
+
         public TradePanelForm()
         {
             InitializeComponent();
